c8a9eea45531f334cf5b653c82b004265dcd8024
OGonzalezNYC <OGonzalezNYC@Gmail.com>
2018-09-22 16:30:04 -0400

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,16 @@
+module Memorable
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
+  # def self.reset_all
+  #   self.all.clear
+  # end
+  #
+  # def self.count
+  #   self.all.count
+  # end
+end
